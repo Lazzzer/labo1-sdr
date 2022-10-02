86547bec190a512c7f779abaed01630690dab4f7
Lazzzer <lazazr@gmail.com>
2022-10-02 22:07:08 +0200

    feat: first version of tcp clients and servers

--- client/client.go
+++ client/client.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/Lazzzer/labo1-sdr/utils"
 )
@@ -9,4 +14,27 @@ import (
 func main() {
 	config := utils.GetConfig("config.json")
 	fmt.Println(config)
+
+	connection, err := net.Dial("tcp", config.Host+":"+strconv.Itoa(config.Port))
+
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Welcome! Please enter a command.\nType 'help' for a list of commands.")
+	}
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print(">> ")
+		text, _ := reader.ReadString('\n')
+		fmt.Fprintf(connection, text+"\n")
+
+		message, _ := bufio.NewReader(connection).ReadString('\n')
+		fmt.Print("From Server -> " + message)
+
+		if strings.TrimSpace(string(text)) == "quit" {
+			fmt.Println("Closing connection")
+			return
+		}
+	}
 }
--- server/server.go
+++ server/server.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net"
+	"strconv"
+	"strings"
 
 	"github.com/Lazzzer/labo1-sdr/utils"
 )
@@ -10,6 +14,34 @@ func main() {
 	config := utils.GetConfig("config.json")
 	fmt.Println(config)
 	users, manifestations := utils.GetEntities("entities.json")
-	fmt.Println(users)
-	fmt.Println(manifestations)
+	fmt.Println(users, manifestations)
+
+	listener, err := net.Listen("tcp", ":"+strconv.Itoa(config.Port))
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+
+	connection, err := listener.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	} else {
+		fmt.Println(connection.LocalAddr().String() + " connected")
+	}
+
+	for {
+		netData, err := bufio.NewReader(connection).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+		}
+		if strings.TrimSpace(string(netData)) == "quit" {
+			fmt.Println("Closing connection to " + connection.LocalAddr().String())
+			return
+		}
+
+		fmt.Print("From Client "+connection.LocalAddr().String()+" -> ", string(netData))
+		connection.Write([]byte(netData + "\n"))
+	}
+
 }
