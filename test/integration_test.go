69f9f2156563fc4b2effa09c49a588d3aff1ce62
Lazzzer <dev@lazarpavicevic.ch>
2022-11-18 21:38:15 +0100

    test: put tests in its own folders and cleanup

--- test/integration_test.go
+++ test/integration_test.go
@@ -1,12 +1,11 @@
 // Auteurs: Jonathan Friedli, Lazar Pavicevic
 // Labo 1 SDR
 
-package client
+package test
 
 import (
 	"fmt"
 	"net"
-	"sync"
 	"testing"
 
 	"github.com/Lazzzer/labo1-sdr/internal/server"
@@ -14,10 +13,15 @@ import (
 	"github.com/Lazzzer/labo1-sdr/internal/utils/types"
 )
 
-var testConfig = types.Config{Address: "localhost:8091", Servers: map[int]string{1: "localhost:8011"}}
-var testingServerConfig = types.ServerConfig{Config: testConfig, Silent: true}
+// TestClient est un client de test
+type TestClient struct {
+	Config types.Config
+}
 
-var testingServerDebugConfig = types.ServerConfig{Config: testConfig, Silent: true, Debug: true, DebugDelay: 10}
+var testConfig = types.Config{Address: "localhost:8091", Servers: map[int]string{1: "localhost:8011"}}
+
+var testClient = TestClient{Config: testConfig}
+var testServer = server.Server{Number: 1, Port: "8011", ClientPort: "8091", Config: types.ServerConfig{Config: testConfig, Silent: true}}
 
 // TestInput définit un test pour un input du client
 type TestInput struct {
@@ -26,18 +30,9 @@ type TestInput struct {
 	Expected    string
 }
 
-// TestClient est un client de test
-type TestClient struct {
-	Config types.Config
-}
-
 // init() lance les serveurs de test
 func init() {
-	serv := server.Server{Number: 1, Port: "8011", ClientPort: "8091", Config: testingServerConfig}
-	// servDebug := server.Server{Number: 2, Port: "8012", ClientPort: "8092", Config: testingServerDebugConfig}
-
-	go serv.Run()
-	// go servDebug.Run()
+	go testServer.Run()
 }
 
 // Run est une méthode de TestClient qui peut accepter plusieurs tests à run
@@ -74,28 +69,7 @@ func (tc *TestClient) Run(tests []TestInput, t *testing.T) {
 	}
 }
 
-// run est une fonction à lancer dans une goroutine pour lancer des tests de clients concurrents
-func run(tc *TestClient, wg *sync.WaitGroup, tests []TestInput, t *testing.T) {
-	defer wg.Done()
-	fmt.Println("Test client is running")
-	tc.Run(tests, t)
-}
-
-// runConcurrent crée des testClients, les fait lancer leurs tests en concurrent et attend qu'ils aient fini
-func runConcurrent(nbClients int, tests [][]TestInput, t *testing.T) {
-	var wg sync.WaitGroup
-	wg.Add(nbClients)
-	for i := 0; i < nbClients; i++ {
-		testClient := TestClient{Config: testConfig}
-		go run(&testClient, &wg, tests[i], t)
-	}
-	wg.Wait()
-	fmt.Println("Test clients are done")
-}
-
 func TestHelpCommand(t *testing.T) {
-	testClient := TestClient{Config: testConfig}
-
 	tests := []TestInput{
 		{
 			Description: "Send help command and receive help message",
@@ -112,8 +86,6 @@ func TestHelpCommand(t *testing.T) {
 }
 
 func TestShowCommand(t *testing.T) {
-	testClient := TestClient{Config: testConfig}
-
 	var showAll = utils.RED + "Closed" + utils.RESET + "\t#1 " + utils.BOLD + utils.CYAN + "Montreux Jazz 2022" + utils.RESET + " / Creator: claude\n\n" +
 		utils.GREEN + "Open" + utils.RESET + "\t#2 " + utils.BOLD + utils.CYAN + "Baleinev 2023" + utils.RESET + " / Creator: john\n\n" +
 		utils.GREEN + "Open" + utils.RESET + "\t#3 " + utils.BOLD + utils.CYAN + "Balélec 2023" + utils.RESET + " / Creator: jane\n"
@@ -146,8 +118,6 @@ func TestShowCommand(t *testing.T) {
 }
 
 func TestJobsCommand(t *testing.T) {
-	testClient := TestClient{Config: testConfig}
-
 	var showJobs = utils.MESSAGE.WrapEvent("#2 \x1b[1m\x1b[36mBaleinev 2023\x1b[0m\n\n\x1b[1mVolunteers\x1b[0m   #1 Montage (2/5)   #2 Stands (2/2)   #3 Sécurité (0/2)   \nvalentin             ✅                                                        \nfrancesco            ✅                                                        \njonathan                                ✅                                     \njane                                    ✅                                     \n")
 
 	var showJobsEmpty = utils.MESSAGE.WrapEvent("#3 \x1b[1m\x1b[36mBalélec 2023\x1b[0m\n\n\x1b[1mVolunteers\x1b[0m   #1 Montage (0/4)   #2 Stands (0/4)   #3 Sécurité (0/1)   \n\nThere is currently no volunteers for this event.\n")
@@ -183,7 +153,6 @@ func TestJobsCommand(t *testing.T) {
 }
 
 func TestCreateCommand(t *testing.T) {
-	testClient := TestClient{Config: testConfig}
 	tests := []TestInput{
 		{
 			Description: "Send create command for event with one job and receive confirmation message",
@@ -220,8 +189,6 @@ func TestCreateCommand(t *testing.T) {
 }
 
 func TestCloseCommand(t *testing.T) {
-	testClient := TestClient{Config: testConfig}
-
 	tests := []TestInput{
 		{
 			Description: "Send close command and receive confirmation message",
@@ -258,8 +225,6 @@ func TestCloseCommand(t *testing.T) {
 }
 
 func TestRegisterCommand(t *testing.T) {
-	testClient := TestClient{Config: testConfig}
-
 	tests := []TestInput{
 		{
 			Description: "Send register command and receive confirmation message",
@@ -324,50 +289,3 @@ func TestRegisterCommand(t *testing.T) {
 	}
 	testClient.Run(tests, t)
 }
-
-// func TestCommandsConcurrentlyNoSharedSection(t *testing.T) {
-
-// 	tests := [][]TestInput{
-// 		{
-// 			{
-// 				Description: "Send show help and receive message",
-// 				Input:       "help\n",
-// 				Expected:    utils.MESSAGE.Help,
-// 			},
-// 		},
-// 		{
-// 			{
-// 				Description: "Send show help and receive message",
-// 				Input:       "help\n",
-// 				Expected:    utils.MESSAGE.Help,
-// 			},
-// 		},
-// 	}
-
-// 	runConcurrent(2, tests, t)
-// }
-
-// func TestCommandsConcurrentlyReadOnly(t *testing.T) {
-
-// 	var message = utils.RED + "Closed" + utils.RESET + "\t#1 " + utils.BOLD + utils.CYAN + "Montreux Jazz 2022" + utils.RESET + " / Creator: claude\n\n" +
-// 		utils.GREEN + "Open" + utils.RESET + "\t#2 " + utils.BOLD + utils.CYAN + "Baleinev 2023" + utils.RESET + " / Creator: john\n\n" +
-// 		utils.GREEN + "Open" + utils.RESET + "\t#3 " + utils.BOLD + utils.CYAN + "Balélec 2023" + utils.RESET + " / Creator: jane\n"
-
-// 	tests := [][]TestInput{
-// 		{
-// 			{
-// 				Description: "Send show command and receive message",
-// 				Input:       "show\n",
-// 				Expected:    utils.MESSAGE.WrapEvent(message),
-// 			},
-// 		},
-// 		{
-// 			{
-// 				Description: "Send show command and receive message",
-// 				Input:       "show\n",
-// 				Expected:    utils.MESSAGE.WrapEvent(message),
-// 			},
-// 		},
-// 	}
-// 	runConcurrent(2, tests, t)
-// }
